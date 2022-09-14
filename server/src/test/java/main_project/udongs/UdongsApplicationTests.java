816df61ab1ae4f4a9bb032bd638288b4fbfcdb7b
daihan-ch22 <daehan.dan.choi@gmail.com>
2022-09-14 18:25:01 +0900

    initial commit

---
+++ server/src/test/java/main_project/udongs/UdongsApplicationTests.java
@@ -0,0 +1,13 @@
+package main_project.udongs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdongsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
