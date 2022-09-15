e1d16c6f91c3c0fda1b745cdf53b486c1a094d1c
daihan-ch22 <daehan.dan.choi@gmail.com>
2022-09-15 12:58:28 +0900

    feat: swagger초기 설정 및 초기테스트 1

---
+++ server/src/main/java/main_project/udongs/swagger/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package main_project.udongs.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-definition")
+                .pathsToMatch("/")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Udongs API")
+                        .description("우리 동네 스터디 프로젝트 API 명세서")
+                        .version("version 0.0.1"));
+    }
+}
