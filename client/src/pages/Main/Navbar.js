e177ee3685386c1f3991ddbc5d2244839933bddd
younha-kim <rladudtmd727@gmail.com>
2022-09-15 11:05:52 +0900

    add mainPage

--- client/src/pages/Main/Navbar.js
+++ client/src/pages/Main/Navbar.js
@@ -1,14 +1,95 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledNav = styled.div`
+  .header-container {
+    height: 63.5px;
+    width: 100%;
+    border: solid black 1px;
+  }
+
+  .nav-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 63.5px;
+    padding-left: 10px;
+  }
+
+  .logo-img {
+    height: 60px;
+  }
+
+  .tap-box {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding-left: 0px;
+    height: 63.5px;
+  }
+  ol {
+    margin: 0px;
+  }
+  li {
+    list-style: none;
+    padding-right: 20px;
+  }
+
+  .other-box {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding-right: 10px;
+  }
+
+  .info-box {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding-left: 20px;
+  }
+`;
 
 const Navbar = () => {
-  return(
+  return (
     <>
-      <div>
-        helloworld
-      </div>
+      <StyledNav>
+        <header className="header-container">
+          <nav className="nav-container">
+            <section className="logo-box">
+              <img
+                className="logo-img"
+                src={require("../../../src/img/logo.png")}
+              />
+            </section>
+
+            <ol className="tap-box">
+              <li>내 주변 스터디</li>
+              <li>내 그룹</li>
+              <li>전체 게시판</li>
+            </ol>
+
+            <section className="other-box">
+              <div className="search-box">
+                <textarea></textarea>
+              </div>
+              <div className="info-box">
+                <div className="new-study-btn">
+                  <button>스터디 생성</button>
+                </div>
+                <div className="alert-btn">
+                  <button>종</button>
+                </div>
+                <div className="my-info-btn">
+                  <button>my info</button>
+                </div>
+              </div>
+            </section>
+          </nav>
+        </header>
+      </StyledNav>
     </>
-  )
-}
+  );
+};
 
-
-export default Navbar 
+export default Navbar;
--- client/src/pages/Main/Notice.js
+++ client/src/pages/Main/Notice.js
@@ -1,14 +1,102 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledNav = styled.div`
+  .nav-container {
+    margin-left: 30px;
+    margin-right: 30px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 50.5px;
+    border: black solid 1px;
+    border-radius: 3%;
+  }
+
+  .notice-box {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 0px;
+    width: 60%;
+  }
+  ol {
+    margin: 0px;
+  }
+  li {
+    list-style: none;
+    padding-right: 20px;
+  }
+
+  .profile-box {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding-left: 0px;
+    height: 63.5px;
+  }
+
+
+  .location-container{
+    width: 25%;
+  }
+  .location-box {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    padding-right: 10px;
+  }
+
+  .info-box {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding-left: 20px;
+  }
+
+  
+`;
 
 const Notice = () => {
-  return(
+  return (
     <>
-      <div>
-        helloworld
-      </div>
+      <StyledNav>
+        <nav className="nav-container">
+          <section className="notice-box">
+            <a>공지사항 입니다!!</a>
+            <div className="notice-btn">
+              <button>Notice</button>
+            </div>
+            <div>
+              <ol className="profile-box">
+                <li>13 Apr, 2022</li>
+                <li>
+                  <button>
+                    profile
+                  </button>
+                </li>
+                <li>kim-young-ha</li>
+              </ol>
+            </div>
+          </section>
+
+          <section className="location-container">
+            <div className="location-box">
+              <div className="now-location">
+                <p>현재 위치</p>
+              </div>
+              <div className="saved-location">
+                <p>서울특별시 서대문구</p>
+              </div>
+              <button className="my-location-btn">밑</button>
+            </div>
+          </section>
+        </nav>
+      </StyledNav>
     </>
-  )
-}
+  );
+};
 
-
-export default Notice 
+export default Notice;
