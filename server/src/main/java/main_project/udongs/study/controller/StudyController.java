8c98b45b0ebaf6086887e919a2209f511457ec71
daihan-ch22 <daehan.dan.choi@gmail.com>
2022-09-20 23:16:19 +0900

    fix: application-sensitive.yml gitignore 추가

---
+++ server/src/main/java/main_project/udongs/study/controller/StudyController.java
@@ -0,0 +1,41 @@
+package main_project.udongs.study.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import main_project.udongs.member.dto.MemberDto;
+import main_project.udongs.member.entity.Member;
+import main_project.udongs.member.service.MemberService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@Slf4j
+@Tag(name = "STUDY", description = "스터디 관련 API")
+@RequestMapping("/study")
+@RequiredArgsConstructor
+public class StudyController {
+
+    private final MemberService memberService;
+
+
+    @Operation(summary = "스터디 모집 등록")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK", content = @Content(array = @ArraySchema(schema = @Schema(implementation = MemberDto.Response.class))))})
+    @PostMapping("/recruit")
+    public ResponseEntity recruitStudyMember()
+    {
+        return new ResponseEntity<>("recruit member", HttpStatus.OK);
+    }
+}
