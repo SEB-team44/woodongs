ae2f394b77faddf94bea4fbd4f8fc66e4bfe8640
daihan-ch22 <daehan.dan.choi@gmail.com>
2022-09-27 15:50:01 +0900

    fix: fetchType / List->Set 변경

--- server/src/main/java/main_project/udongs/member/entity/Member.java
+++ server/src/main/java/main_project/udongs/member/entity/Member.java
@@ -14,7 +14,9 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -93,22 +95,22 @@ public class Member {
     //스터디 신청 목록
     @Builder.Default
     @OneToMany(mappedBy = "member")
-    private List<StudyApply> studyApplies = new ArrayList<>();
+    private Set<StudyApply> studyApplies = new HashSet<>();
 
     //내가 속한 스터디(그룹장)
     @Builder.Default
     @OneToMany(mappedBy = "member", fetch = FetchType.EAGER)
-    private List<Study> studies = new ArrayList<>();
+    private Set<Study> studies = new HashSet<>();
 
     //내가 속한 스터디(멤버)
     @Builder.Default
     @OneToMany(mappedBy = "member", fetch = FetchType.EAGER)
-    private List<Acceptance> acceptances = new ArrayList<>();
+    private Set<Acceptance> acceptances = new HashSet<>();
 
     //스터디 모집글 질문(comment)
     @Builder.Default
-    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
-    private List<StudyComment> comments = new ArrayList<>();
+    @OneToMany(mappedBy = "member")
+    private Set<StudyComment> comments = new HashSet<>();
 
     //시큐리티 상에서 필요한 로직(member를 새로 만들어야 하는 경우가 있음)
     public Member(
