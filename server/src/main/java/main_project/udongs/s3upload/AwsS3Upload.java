66f850a30fbdf88c1e10fecf77178f1a2fc7592a
daihan-ch22 <daehan.dan.choi@gmail.com>
2022-09-28 15:25:57 +0900

    feat: s3업로드시 이전이미지가 있으면 삭제후 등록

--- server/src/main/java/main_project/udongs/s3upload/AwsS3Upload.java
+++ server/src/main/java/main_project/udongs/s3upload/AwsS3Upload.java
@@ -15,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 @Service
@@ -28,7 +30,7 @@ public class AwsS3Upload {
     private final AmazonS3 amazonS3;
     private final MemberRepository memberRepository;
 
-    @Transactional
+
     public String upload(MultipartFile multipartFile, Member member) throws IOException {
 
         //이미 이미지가 등록되있으면 그거 삭제후 등록
@@ -44,37 +46,32 @@ public class AwsS3Upload {
         objMeta.setContentLength(multipartFile.getInputStream().available());
 
         //이미지 업로드
-                amazonS3.putObject(bucket, s3FileName, multipartFile.getInputStream(), objMeta);
+        amazonS3.putObject(bucket, s3FileName, multipartFile.getInputStream(), objMeta);
+
+        log.debug("Successfully uploaded:{} ", s3FileName );
 
         //업로드 된 이미지의 url 정보 String으로 반환
         return amazonS3.getUrl(bucket, s3FileName).toString();
     }
 
-    // 지우는거 고민해보기
-    // 어떻게 KEY값을 받아올지??
-    // 뒷부분이 %%%이런식으로 나오는데 제대로 나와야함.
-    //
-    @Transactional
+
     private void delete(Member member) {
 
             String s = member.getProfileImageUrl();
             AmazonS3URI s3URI = new AmazonS3URI(s);
 
-            String key = s3URI.getKey();
-            System.out.println(key);
+            String key = URLDecoder.decode((s3URI.getKey().toString()), StandardCharsets.UTF_8);
 
-            if(amazonS3.doesObjectExist("bucket", key)) {
-                amazonS3.deleteObject(new DeleteObjectRequest(s3URI.getBucket(), s3URI.getKey()));
-                log.debug("s3 image DELETED");
-
-                /*try {
-                    amazonS3.deleteObject(bucket, key);
+                try {
+                    DeleteObjectRequest deleteRequest = new DeleteObjectRequest(bucket, key);
+                    amazonS3.deleteObject(deleteRequest);
+                    log.debug("Deleted previous image:{} ", key );
                 } catch(Exception e) {
                     e.printStackTrace();
                     log.error("Exception ERROR: {} ", e.getMessage());
                     throw e;
-                }*/
-            }
+                }
+
         }
     }
 
