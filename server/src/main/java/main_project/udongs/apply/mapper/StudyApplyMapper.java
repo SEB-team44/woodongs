7574c9c172f3b996ac84efef922dd75c811cde1d
rhldkszns97 <sujeng98@naver.com>
2022-09-28 17:47:25 +0900

    add admin page

--- server/src/main/java/main_project/udongs/apply/mapper/StudyApplyMapper.java
+++ server/src/main/java/main_project/udongs/apply/mapper/StudyApplyMapper.java
@@ -1,10 +1,9 @@
 package main_project.udongs.apply.mapper;
 
-import main_project.udongs.apply.dto.StudyApplyResponseDto;
+import main_project.udongs.apply.dto.StudyApplyDto;
 import main_project.udongs.apply.entity.StudyApply;
 import main_project.udongs.member.dto.MemberDto;
-import main_project.udongs.study.dto.StudyDto;
-import main_project.udongs.study.entity.Study;
+import main_project.udongs.member.entity.Member;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
@@ -13,7 +12,34 @@ import java.util.List;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface StudyApplyMapper {
 
-    Study studyApplyPostToStudy(StudyDto.Post requestBody);
+//    Study studyApplyPostToStudy(StudyDto.Post requestBody);
+
+    default StudyApplyDto.Response studyApplyToStudyApplyResponse(StudyApply studyApply) {
+        Member member = studyApply.getMember();
+
+        MemberDto.Response memberResponseDto = MemberDto.Response.builder()
+                .memberId(member.getMemberId())
+                .nickName(member.getNickName())
+                .email(member.getEmail())
+                .phoneNumber(member.getPhoneNumber())
+                .city(member.getCity())
+                .emailVerifiedYn(member.getEmailVerifiedYn())
+                .profileImageUrl(member.getProfileImageUrl())
+                .providerType(member.getProviderType())
+                .roleType(member.getRoleType())
+                .latitude(member.getLatitude())
+                .longitude(member.getLongitude())
+                .createdAt(member.getCreatedAt())
+                .modifiedAt(member.getModifiedAt())
+                .build();
+
+        return StudyApplyDto.Response.builder()
+                .studyApplyId(studyApply.getStudyApplyId())
+                .state(studyApply.getState())
+                .memberResponseDto(memberResponseDto)
+                .build();
+    }
+    List<StudyApplyDto.Response> studyAppliesToStudyApplyResponse(List<StudyApply> studyApplies);
 
 //    default StudyApplyResponseDto StudyApplyToStudyApplyResponseDto(StudyApply studyApply) {
 //        MemberDto.Response.builder()
--- server/src/main/java/main_project/udongs/study/controller/StudyController.java
+++ server/src/main/java/main_project/udongs/study/controller/StudyController.java
@@ -9,19 +9,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import main_project.udongs.apply.service.StudyApplyService;
-
 import main_project.udongs.globaldto.MultiResponseDto;
-
-import main_project.udongs.member.dto.MemberDto;
 import main_project.udongs.member.entity.Member;
 import main_project.udongs.member.service.MemberService;
-
-import main_project.udongs.study.dto.SingleResponseStudyDto;
-
 import main_project.udongs.oauth2.oauth.entity.UserPrincipal;
-
+import main_project.udongs.study.dto.SingleResponseStudyDto;
 import main_project.udongs.study.dto.StudyCommentDto;
 import main_project.udongs.study.dto.StudyDto;
 import main_project.udongs.study.entity.Study;
@@ -32,7 +25,6 @@ import main_project.udongs.study.service.StudySearchService;
 import main_project.udongs.study.service.StudyService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
@@ -112,6 +104,7 @@ public class StudyController {
 
         return ResponseEntity.ok(mapper.studyToStudyResponse(study));
     }
+
 
 
     //무한스크롤
