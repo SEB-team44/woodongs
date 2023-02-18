058da1277444e7a2b74ad094447aa0a4d60120db
younha-kim <rladudtmd727@gmail.com>
2023-02-18 22:20:03 +0900

    fix getlocation

--- client/src/pages/Main/divider.js
+++ client/src/pages/Main/divider.js
@@ -4,13 +4,16 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
 import { Link } from "react-router-dom";
-
+import { useContext } from "react";
+import { UserInfo } from "../../UserContext";
 const style = {
   width: "100%",
   bgcolor: "background.paper",
 };
 
 export default function ListDividers() {
+  const { userInfo } = useContext(UserInfo);
+
   return (
     <List sx={style} component="nav" aria-label="mailbox folders">
       <Link to="/EntireMain">
@@ -20,12 +23,21 @@ export default function ListDividers() {
         <Divider />
       </Link>
 
-      <Link to="/main">
-        <ListItem button divider>
-          <ListItemText primary="내 주변 스터디" />
-        </ListItem>
-        <Divider light />
-      </Link>
+      {userInfo.city ? (
+        <Link to="/main">
+          <ListItem button divider>
+            <ListItemText primary="내 주변 스터디" />
+          </ListItem>
+          <Divider light />
+        </Link>
+      ) : (
+        <Link>
+          <ListItem button divider>
+            <ListItemText primary="내 주변 스터디" sx={{color : "lightgray"}} />
+          </ListItem>
+          <Divider light />
+        </Link>
+      )}
 
       <Link to="/FreeBoard">
         <ListItem button>
